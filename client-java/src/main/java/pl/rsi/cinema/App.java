226d7c3e194015bf039413f723815044ef097ec8
Hackeristi <klaudia.n.klim@gmail.com>
2026-04-26 19:06:00 +0200

    Utworzono szkielety serwera .NET i klienta Java

---
+++ client-java/src/main/java/pl/rsi/cinema/App.java
@@ -0,0 +1,13 @@
+package pl.rsi.cinema;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

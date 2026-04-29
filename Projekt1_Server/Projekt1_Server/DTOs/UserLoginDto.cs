65ea1d3f82b71f769c096c2ec0b6162847b0c658
karichil <135642850+karichil@users.noreply.github.com>
2026-04-29 15:53:53 +0200

    Revert "poprawiona rejestracja i logowanie"

--- Projekt1_Server/Projekt1_Server/DTOs/UserLoginDto.cs
+++ Projekt1_Server/Projekt1_Server/DTOs/UserLoginDto.cs
@@ -4,5 +4,5 @@ public class UserLoginDto
 {
     public string UserName { get; set; }
     public string Email { get; set; }
-    public string ErrorMessage { get; set; }
+    public string Password { get; set; }
 }
